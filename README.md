c962d14c7e3e991fa3ced09d0bb7bec82fd4e07e
Bas van der Hoek <bas@hoek.com>
2023-03-18 12:35:40 +0100

    start with ast

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# picturs: pic grammar in Rust
+
+* https://pikchr.org/home/doc/trunk/homepage.md
+* https://pikchr.org/home/doc/trunk/doc/grammar.md
+
+## pest
+ 
+* https://docs.rs/pest/latest/pest/
+* https://pest.rs/book/intro.html
+* https://github.com/pest-parser/pest
+
+https://blog.logrocket.com/understanding-rust-option-results-enums/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::error::Error;
 use std::fs;
 
-use pest::error::Error;
+use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -8,19 +9,55 @@ use pest_derive::Parser;
 #[grammar = "pic.pest"] // relative to project `src`
 struct PicParser;
 
-fn main() {
-  let pic = fs::read_to_string("pikchr.pic").unwrap();
-  match PicParser::parse(Rule::statements, &*pic) {
-    Ok(mut pairs) => {
-      let next = pairs.next().unwrap();
-      for pair in next.into_inner() {
-        println!("{:?}", pair);
-      }
-      // println!("{:?}", pairs);
-    }
-    Err(e) => { println!("{}", e) }
-  };
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub enum ShapeType {
+  Arrow,
+  Box,
 }
+
+#[derive(Debug)]
+struct Shape {
+  class: Option<ShapeType>,
+  text: Vec<String>,
+}
+
+fn object_definition(pair: Pair<Rule>, mut shape: Shape) -> Shape {
+  for pair in pair.into_inner() {
+    match pair.as_rule() {
+      Rule::object_class => {
+        shape.class = Option::from(match pair.as_str() {
+          "arrow" => ShapeType::Arrow,
+          "box" => ShapeType::Box,
+          _ => unreachable!()
+        })
+      }
+      Rule::attribute => {
+        shape = object_definition(pair, shape);
+      }
+      Rule::string => {
+        shape.text.push(pair.into_inner().as_str().to_string());
+      }
+      _ => { println!("{:?}", pair) }
+    }
+  }
+  shape
+}
+
+fn main() {
+  let string = fs::read_to_string("pikchr.pic").unwrap();
+  let pairs = PicParser::parse(Rule::picture, &*string).unwrap();
+
+  for pair in pairs {
+    for inner in pair.into_inner() {
+      let mut shape = Shape { class: None, text: Vec::new() };
+      let shape = object_definition(inner, shape);
+      println!("{:?}", shape);
+    }
+  }
+}
+
 
 #[cfg(test)]
 mod tests {
